2ae37d0685b314ed914774f4939b2b46a8680a11
Jason Jiang <ssdog27@gmail.com>
2020-09-14 18:03:16 +0800

    configure Devise with User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  authenticated :user do
+    root to: 'projects#index', as: :authenticated_root
+  end
+
   resources :projects
   root "home#index"
 end
