bb8c2038b4d21e903fa129a4bb304918b44acf2a
Jason Jiang <ssdog27@gmail.com>
2020-09-15 03:16:54 +0800

    add Tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_103755) do
+ActiveRecord::Schema.define(version: 2020_09_14_180956) do
 
   create_table "notes", force: :cascade do |t|
     t.text "message"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2020_09_14_103755) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.boolean "completed"
+    t.integer "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +63,5 @@ ActiveRecord::Schema.define(version: 2020_09_14_103755) do
   add_foreign_key "notes", "projects"
   add_foreign_key "notes", "users"
   add_foreign_key "projects", "users"
+  add_foreign_key "tasks", "projects"
 end
