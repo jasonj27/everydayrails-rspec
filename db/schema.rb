62153186b65dc3d4d82614c32dd4869366386610
Jason Jiang <ssdog27@gmail.com>
2020-09-20 17:05:16 +0800

    Find user locations by IP address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_202409) do
+ActiveRecord::Schema.define(version: 2020_09_20_090257) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_202409) do
     t.string "first_name"
     t.string "last_name"
     t.string "authentication_token"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
