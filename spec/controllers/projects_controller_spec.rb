e64903186f16b94683535e7dd88d7522141c65cd
Jason Jiang <ssdog27@gmail.com>
2020-09-26 00:14:27 +0800

    Test projects controller, add #new, #edit

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -68,6 +68,53 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#new" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+  
+      it "responds successfully" do
+        sign_in @user
+        get :new
+  
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe "#edit" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+  
+      it "responds successfully" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+  
+        expect(response).to be_successful
+      end
+    end
+
+    context "as a unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      it "redirects to dashboard" do
+        sign_in @user
+        get :edit params: { id: @project.id }
+
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
   describe "#create" do
     context "as an authenticated user" do
       before do
