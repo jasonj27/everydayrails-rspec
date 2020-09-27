4c5cddbaa63652e0b823fcdf511e0493bffcfe5d
Jason Jiang <ssdog27@gmail.com>
2020-09-28 01:20:06 +0800

    Fix typo in geocode_user_job

--- spec/jobs/geocoder_user_job_spec.rb
+++ spec/jobs/geocoder_user_job_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe GeocoderUserJob, type: :job do
+RSpec.describe GeocodeUserJob, type: :job do
   it "calls geocode on the user" do
     user = instance_double("User")
     expect(user).to receive(:geocode)
