437b254be469394b72a5c611c01972c9bde242ea
Jason Jiang <ssdog27@gmail.com>
2020-09-27 21:22:23 +0800

    Refactor user and project model spec using shoulda-matchers

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,50 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
+  it { should validate_presence_of :name }
 
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-    )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
-
-  it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    other_project = other_user.projects.build(
-      name: "Test Project",
-    )
-
-    expect(other_project).to be_valid
+  it "is expected to validate that :name is unique within the scope of :user_id" do
+    FactoryBot.create(:project)
+    should validate_uniqueness_of(:name).scoped_to(:user_id)
   end
 
   describe "late status" do
